e01e953612641260b0a71d1122de28d5310916c6
Farkas Benedek <farkas.zsb@gmail.com>
2012-04-12 12:20:57 +0200

    AdminMenu + temp Index view

---
+++ AdminMenu.cs
@@ -0,0 +1,18 @@
+﻿using Orchard.Localization;
+using Orchard.UI.Navigation;
+
+namespace OrchardHUN.ModuleProfiles
+{
+    public class AdminMenu : INavigationProvider
+    {
+        public Localizer T { get; set; }
+        public string MenuName { get { return "admin"; } }
+
+        public void GetNavigation(NavigationBuilder builder)
+        {
+            builder.Add(T("Modules"),
+                menu => menu.Add(T("Module Profiles"), "4", item => item.Action("Index", "Admin", new { area = "OrchardHUN.ModuleProfiles" }).LocalNav())
+            );
+        }
+    }
+}
---
+++ Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using Orchard;
+using Orchard.Environment.Extensions;
+using Orchard.Localization;
+
+namespace OrchardHUN.ModuleProfiles.Controllers
+{
+    [OrchardFeature("OrchardHUN.ModuleProfiles")]
+    public class AdminController : Controller
+    {
+        private readonly IOrchardServices _orchardServices;
+
+        public Localizer T { get; set; }
+
+        public AdminController(
+            IOrchardServices orchardServices)
+        {
+            _orchardServices = orchardServices;
+
+            T = NullLocalizer.Instance;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
